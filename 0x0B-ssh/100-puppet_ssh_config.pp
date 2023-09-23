8b32ba016a57b343c7612327a507ffd340bd28ee
oumatoulacen <oumatoulacen@gmail.com>
2023-09-24 00:49:14 +0100

    Client configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
 file { '/home/ubuntu/.ssh/ssh_config':
 	ensure => 'file',
 	owner => 'ubuntu',
-	group => 'ubuntu',
-	mode => '0744',
 	content => "
-		host 100.25.163.174
+		# SSH client configuration
+		Host *
 		IdentityFile ~/.shh/school
 		PasswordAuthentication no
 	"
