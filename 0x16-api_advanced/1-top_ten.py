a5a5f9bfc8dc0a7a6d9d90b3035df7269dbcd3c2
oumatoulacen <oumatoulacen@gmail.com>
2023-11-08 02:32:08 +0100

    prints the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-'''function that queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
+'''prints the titles of the first 10 hot posts listed for a given subreddit.
 '''
 import requests
 
