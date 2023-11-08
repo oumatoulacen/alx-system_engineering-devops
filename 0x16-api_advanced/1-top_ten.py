8b70494017cf599b106b6a4cf4ae11e05581461b
oumatoulacen <oumatoulacen@gmail.com>
2023-11-08 02:30:58 +0100

    prints the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+'''function that queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit.
+'''
+import requests
+
+
+def top_ten(subreddit):
+    '''prints the titles of the first 10 hot posts'''
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    res = requests.get(url)
+    if res.status_code == 200:
+        data = res.json()['data']
+        for post in data:
+            print(post['title'])
+    else:
+        print("None")
