c13c0d896617968590e210b05d16df836c2f98d9
oumatoulacen <oumatoulacen@gmail.com>
2023-11-08 03:18:26 +0100

    prints the titles of the first 10 hot posts

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''recursive function returns titles of all hot articles of a subreddit'''
+import requests
+
+global prev;
+def recurse(subreddit, hot_list=[]):
+    ''' a recursive function that queries the Reddit API '''
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    res = requests.get(url)
+    if res.status_code == 200:
+        all_data = res.json()['data']
+        for new in range(len(all_data)):
+            hot_list.append(all_data[new]['title'])
+        return hot_list.extend(recurse(subreddit, hot_list))
+    else:
+        return None
